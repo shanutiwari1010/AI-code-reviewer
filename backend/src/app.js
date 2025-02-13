8dfffde8c403099928c7675cef7ef9ce7fb2b715
Shanu Tiwari <shanutiwari.work@gmail.com>
2025-02-13 23:25:56 +0530

    updated code

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,12 @@
 import express from "express";
 import { router } from "./routes/ai.routes.js"; // Ensure correct import
+import cors from 'cors'
 
 const app = express();
 
+
 app.use(express.json());
+app.use(cors())
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "prismjs/themes/prism-tomorrow.css";
+import Editor from "react-simple-code-editor";
+import prism from "prismjs";
+import axios from "axios";
+import Markdown from "react-markdown";
 
-function App() {
-  const [count, setCount] = useState(0)
+import rehypeHighlight from "rehype-highlight";
+import "highlight.js/styles/github-dark.css";
+
+const App = () => {
+  const [code, setCode] = useState(`function sum (){
+    return 1+1}`);
+
+  const [review, setReview] = useState("");
+  useEffect(() => {
+    prism.highlightAll();
+  }, []);
+
+  async function reviewCode() {
+    const response = await axios.post("http://localhost:3000/ai/get-review", {
+      code,
+    });
+
+    console.log(response.data, "response");
+    setReview(response.data);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main className="grid grid-cols-2 w-full min-h-screen bg-[#343434] p-6 gap-3">
+        <div className="bg-black rounded-xl flex flex-col justify-between border border-[#ddd]">
+          <div className="text-white">
+            <Editor
+              value={code}
+              onValueChange={(code) => setCode(code)}
+              highlight={(code) =>
+                prism.highlight(code, prism.languages.javascript, "javascript")
+              }
+              padding={10}
+              style={{
+                fontFamily: '"Fira code", "Fira Mono", monospace',
+                fontSize: 16,
+                borderRadius: "5px",
+                height: "100%",
+                width: "100%",
+              }}
+            />
+          </div>
+          <div className="flex items-end justify-end border-0 p-4">
+            <button
+              onClick={reviewCode}
+              className="bg-white px-6 cursor-pointer py-2 rounded-lg"
+            >
+              Review
+            </button>
+          </div>
+        </div>
+        <div className="bg-[#484545] rounded-xl p-3 text-white overflow-auto">
+          <Markdown rehypePlugins={[rehypeHighlight]}>{review}</Markdown>
+        </div>
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
